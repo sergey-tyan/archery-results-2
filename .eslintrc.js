9ea5371bdb8443764dcd768df97fc22adb09af30
setyan <sergey.tyan.iitu@gmail.com>
2018-02-18 17:52:00 +0600

    git initial

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    parser: 'babel-eslint',
+    extends: ['airbnb', 'prettier', 'prettier/flowtype'],
+    plugins: ['react', 'jsx-a11y', 'import', 'jest'],
+    rules: {
+        indent: ['error', 4],
+        'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+        'react/jsx-indent': ['error', 4],
+        'react/jsx-indent-props': ['error', 4],
+        'comma-dangle': ['error', 'never'],
+        'react/no-multi-comp': 0,
+        'react/sort-comp': 0,
+        'class-methods-use-this': 0,
+        'no-unused-vars': ['warn'],
+        'import/prefer-default-export': ['warn'],
+        'prefer-destructuring': 0
+    },
+    env: {
+        'jest/globals': true
+    },
+    globals: {
+        Generator: true,
+        $Shape: true
+    }
+};
